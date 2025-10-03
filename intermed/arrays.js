c7e3e4d4086eba5bec83f9c2ad6ecb3e68739490
Viddhhyyaaa <142838642+Viddhhyyaaa@users.noreply.github.com>
2025-10-03 16:47:58 +0000

    object practice

--- intermed/arrays.js
+++ intermed/arrays.js
@@ -2,3 +2,5 @@ const myarray=[0,1,2,3,4,5,true,"abc"]
 // console.log(myarray[2])
 myarray.push(6)
 console.log(myarray)
+myarray.unshift(9)
+console.log(myarray)
---
+++ intermed/obj2.js
@@ -0,0 +1,9 @@
+//object singleton
+// //const tinderuser= new Object() SINGLETON OBJECT
+// const tinderuser= {}  //NON-SINGLETON OBJECT(LITERALS)
+// console.log(tinderuser);
+const tinderuser= {}
+tinderuser.id= "123abc"
+tinderuser.name= "vidhya"
+tinderuser.isloggedin= false
+console.log(tinderuser)
