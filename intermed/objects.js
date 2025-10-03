83817f3677703d80525aec7e74d3d971f37015b4
Viddhhyyaaa <142838642+Viddhhyyaaa@users.noreply.github.com>
2025-10-03 16:52:02 +0000

    added object.js file

---
+++ intermed/objects.js
@@ -0,0 +1,18 @@
+//object 
+const jsuser={
+    name: "vidhya",
+    age: 18,
+    location: "noida",
+    email: "raividhya12@gmail.com",
+    isloggedin: false,
+    lastlogindays: ["monday", "saturday"]
+}
+
+// console.log(jsuser.email)
+// console.log(jsuser["email"])
+// //name, age , email etc are goin as string
+// //therefore we can't write jsuser[email] directly
+// //we have to write  it like jsuser["email"]
+//  jsuser.email= "vidhyaaaa@gmail.com"
+//  Object.freeze(jsuser)
+//  console.log(jsuser)
